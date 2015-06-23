e724daa7b70e3d5b14289e10eda7697f25e3b08d
maxbilbow <maxbilbow@gmail.com>
2015-06-23 13:35:34 +0100

    Stop waisting time!

---
+++ ADHDSimUnity/Assets/ADHDSim/Scripts/ClockBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ADHD {
+	public class ClockBehaviour : RMXGameObject {
+		// Use this for initialization
+		public Vector3 startingPoint;
+		public GameObject goAwayText;
+		void Start () {
+			if (startingPoint == Vector3.zero && position != Vector3.zero) {
+				startingPoint = position;
+			}
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			if (fellOffTheScreen && !goAwayText.activeInHierarchy) {
+				goAwayText.GetComponent<GoAwayText>().ActivateWithObject(this);
+			}
+		}
+
+		public bool fellOffTheScreen {
+			get {
+				return !GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(rmx.activeCamera), gameObject.GetComponent<Collider2D>().bounds);
+//				return ( self.point.y < self.ma
+			}
+		}
+
+		public override void Reset() {
+			transform.position = startingPoint;
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		}
+
+	}
+}
---
+++ ADHDSimUnity/Assets/ADHDSim/Scripts/GoAwayText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace ADHD {
+	public class GoAwayText : RMXGameObject, IPointerClickHandler {
+
+		RMXGameObject objectToReset;
+		// Use this for initialization
+//		void Start () {
+//		
+//		}
+		
+		public void ActivateWithObject(RMXGameObject obj) {
+			objectToReset = obj;
+			Activate();
+		}
+
+		public void OnPointerClick(PointerEventData data) {
+			objectToReset.Reset();
+			Deactivate();
+		}
+	}
+
+}
