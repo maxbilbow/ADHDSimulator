431644918c59b4ddf0e83bbf574e6af7744d4f67
maxbilbow <maxbilbow@gmail.com>
2015-07-07 13:32:02 +0100

    gyro!

---
+++ ProCrastinate/Assets/ADHDSim/Scripts/Gyro.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RMX {
+	public class Gyro : MonoBehaviour {
+
+		void Update () 
+		{
+			Physics2D.gravity = new Vector2 (Input.acceleration.x, Input.acceleration.y);
+		}
+	}
+}
