0bce8ae24df5a82ecc5fa2119358d52a2ecac110
maxbilbow <maxbilbow@gmail.com>
2015-07-26 12:45:15 +0100

    update to achievement reporting.

--- Pro-Crastinate/Assets/Plugins/GKAchievementReporter/GKAchievementReporter.cs
+++ Pro-Crastinate/Assets/Plugins/GKAchievementReporter/GKAchievementReporter.cs
@@ -11,15 +11,24 @@ namespace UnityEngine.SocialPlatforms {
 		/// Reports an achievement natively if running Game Center on iOS. Otherwise using Social.ReportProgress
 		/// </summary>
 		public static void ReportAchievement(string achievementID, float progress, bool showsCompletionBanner) {
-			if (Social.Active is GameCenter.GameCenterPlatform && Application.platform == RuntimePlatform.IPhonePlayer) {
 
-				// Use native iOS code if running GameCenter on an iOS device
-				_ReportAchievement(achievementID, progress, showsCompletionBanner);
+			if (Social.Active is GameCenter.GameCenterPlatform && Application.platform == RuntimePlatform.IPhonePlayer) {
+				try {
+					// Use native iOS code if running GameCenter on an iOS device
+					_ReportAchievement(achievementID, progress, showsCompletionBanner);
+				} catch (System.Exception e) {
+					if (Bugger.WillLog(RMXTests.Exceptions,e.Message) || Bugger.WillLog(RMXTests.GameCenter,e.Message) )
+						Debug.Log(Bugger.Last);
+				}
 
 			} else {
 				// Otherwise trust the Unity Social API
-
-				Social.ReportProgress(achievementID, progress, null );
+				try { 
+					Social.ReportProgress(achievementID, progress, null );
+				} catch (System.Exception e) {
+					if (Bugger.WillLog(RMXTests.Exceptions,e.Message) || Bugger.WillLog(RMXTests.GameCenter,e.Message) )
+						Debug.Log(Bugger.Last);
+				}
 //				success => {
 //					if (!success) {
 //						var message = achievementID + " Not Found";
