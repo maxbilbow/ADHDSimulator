927265310acd5afe280b6362de27eae7d58e6614
maxbilbow <max@rattlemedia.co.uk>
2015-11-05 22:19:34 +0000

    v0.7 ready

---
+++ Assets/Pro-Crastinate/javascript/NewBehaviourScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+
+//var amqp = RequireComponent("amqplib/callback_api");
+function Start () {
+//	amqp.connect('amqp://localhost', function(err, conn) {});
+}
+
+function Update () {
+
+}
+
